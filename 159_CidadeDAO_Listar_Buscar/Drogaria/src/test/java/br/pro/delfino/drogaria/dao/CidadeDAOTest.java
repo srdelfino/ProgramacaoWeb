979e05202b2c7feb62241e0ddaad97d8894d0b5b
srdelfino <srdelfino@gmail.com>
2015-08-27 16:42:03 -0300

    Alterações
    Alterações

---
+++ 159_CidadeDAO_Listar_Buscar/Drogaria/src/test/java/br/pro/delfino/drogaria/dao/CidadeDAOTest.java
@@ -0,0 +1,56 @@
+package br.pro.delfino.drogaria.dao;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.pro.delfino.drogaria.domain.Cidade;
+import br.pro.delfino.drogaria.domain.Estado;
+
+public class CidadeDAOTest {
+	@Test
+	@Ignore
+	public void salvar() {
+		EstadoDAO estadoDAO = new EstadoDAO();
+
+		Estado estado = estadoDAO.buscar(1L);
+
+		Cidade cidade = new Cidade();
+		cidade.setNome("Marília");
+		cidade.setEstado(estado);
+
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		cidadeDAO.salvar(cidade);
+	}
+
+	@Test
+	@Ignore
+	public void listar() {
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		List<Cidade> resultado = cidadeDAO.listar();
+
+		for (Cidade cidade : resultado) {
+			System.out.println("Código da Cidade: " + cidade.getCodigo());
+			System.out.println("Nome da Cidade: " + cidade.getNome());
+			System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+			System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+			System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+			System.out.println();
+		}
+	}
+	
+	@Test
+	public void buscar(){
+		Long codigo = 7L;
+		
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+		
+		System.out.println("Código da Cidade: " + cidade.getCodigo());
+		System.out.println("Nome da Cidade: " + cidade.getNome());
+		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo());
+		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla());
+		System.out.println("Nome do Estado: " + cidade.getEstado().getNome());
+	}
+}
