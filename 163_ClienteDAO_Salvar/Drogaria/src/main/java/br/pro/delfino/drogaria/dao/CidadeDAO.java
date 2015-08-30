7180bc6ba18bdfad8b94760b5ff1d41778527be2
srdelfino <srdelfino@gmail.com>
2015-08-30 18:11:00 -0300

    Alterações

---
+++ 163_ClienteDAO_Salvar/Drogaria/src/main/java/br/pro/delfino/drogaria/dao/CidadeDAO.java
@@ -0,0 +1,7 @@
+package br.pro.delfino.drogaria.dao;
+
+import br.pro.delfino.drogaria.domain.Cidade;
+
+public class CidadeDAO extends GenericDAO<Cidade> {
+
+}
---
+++ 164_UsuarioDAO_Salvar/Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Usuario.java
@@ -0,0 +1,55 @@
+package br.pro.delfino.drogaria.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@SuppressWarnings("serial")
+@Entity
+public class Usuario extends GenericDomain {
+	@Column(length = 32, nullable = false)
+	private String senha;
+	
+	@Column(nullable = false)
+	private Character tipo;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+	
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Pessoa pessoa;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Character getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Character tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+}
