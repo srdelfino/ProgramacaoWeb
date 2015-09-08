a66b71b6fde88b56ca4e0cbb6c4447c91d98e395
srdelfino <srdelfino@gmail.com>
2015-09-08 17:00:38 -0300

    Alterações

---
+++ 169_Tela_Estado_Listagem/Drogaria/src/main/java/br/pro/delfino/drogaria/dao/FabricanteDAO.java
@@ -0,0 +1,7 @@
+package br.pro.delfino.drogaria.dao;
+
+import br.pro.delfino.drogaria.domain.Fabricante;
+
+public class FabricanteDAO extends GenericDAO<Fabricante> {
+
+}
---
+++ 170_Tela_Estado_Cadastro/Drogaria/src/main/java/br/pro/delfino/drogaria/domain/GenericDomain.java
@@ -0,0 +1,24 @@
+package br.pro.delfino.drogaria.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass
+public class GenericDomain implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long codigo;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+}
