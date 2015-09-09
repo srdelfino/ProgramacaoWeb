2f767038487f15b2d4d5469f208cb12e5717f305
srdelfino <srdelfino@gmail.com>
2015-09-09 17:55:50 -0300

    Alterações

---
+++ 177_OmniFaces/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/EstadoBean.java
@@ -0,0 +1,12 @@
+package br.pro.delfino.drogaria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+import org.omnifaces.util.Messages;
+
+@ManagedBean
+public class EstadoBean {
+	public void salvar(){
+		Messages.addGlobalError("Programação Web com Java");
+	}
+}
