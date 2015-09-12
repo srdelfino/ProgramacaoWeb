efb8c662c48e22e12ea6c29a1bfb07995e7543dc
srdelfino <srdelfino@gmail.com>
2015-09-12 19:35:22 -0300

    Alterações

---
+++ 182_EstadoBean_Ordenar_Filtrar_Paginar/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/EstadoBean.java
@@ -0,0 +1,66 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.EstadoDAO;
+import br.pro.delfino.drogaria.domain.Estado;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class EstadoBean implements Serializable {
+	private Estado estado;
+	private List<Estado> estados;
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	public List<Estado> getEstados() {
+		return estados;
+	}
+	
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+	
+	@PostConstruct
+	public void listar(){
+		try{
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os estados");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+		estado = new Estado();
+	}
+
+	public void salvar() {
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.salvar(estado);
+
+			novo();
+
+			Messages.addGlobalInfo("Estado salvo com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o estado");
+			erro.printStackTrace();
+		}
+	}
+}
