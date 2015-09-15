ea49d64d814de9e6b6345052a0a2a96cf9c0c66b
srdelfino <srdelfino@gmail.com>
2015-09-15 00:48:57 -0300

    Alterações

---
+++ 190_CidadeBean_Novo/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/CidadeBean.java
@@ -0,0 +1,52 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.CidadeDAO;
+import br.pro.delfino.drogaria.domain.Cidade;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class CidadeBean implements Serializable {
+	private Cidade cidade;
+	private List<Cidade> cidades;
+	
+	public Cidade getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+	
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+	
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+	
+	@PostConstruct
+	public void listar(){
+		try{
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidades = cidadeDAO.listar();
+		}catch(RuntimeException erro){
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar listar as cidades");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void novo(){
+		cidade = new Cidade();
+	}
+}
