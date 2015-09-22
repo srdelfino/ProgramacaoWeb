2ec6468155e3cbac6ed11822a525b5a1cd14762f
srdelfino <srdelfino@gmail.com>
2015-09-22 11:43:35 -0300

    Alterações

---
+++ 202_Pessoa_Errata/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/PessoaBean.java
@@ -0,0 +1,56 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import br.pro.delfino.drogaria.domain.Pessoa;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class PessoaBean implements Serializable {
+	private Pessoa pessoa;
+	private List<Pessoa> pessoas;
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+	
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	@PostConstruct
+	public void listar() {
+
+	}
+
+	public void novo() {
+
+	}
+
+	public void editar(ActionEvent evento) {
+
+	}
+
+	public void salvar() {
+
+	}
+
+	public void excluir(ActionEvent evento) {
+
+	}
+}
