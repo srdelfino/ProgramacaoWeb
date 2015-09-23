5c0a4ac122054949865364c1f3349f914db8d12a
srdelfino <srdelfino@gmail.com>
2015-09-23 16:01:41 -0300

    Alterações

---
+++ 205_Listar_Ordenado/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/PessoaBean.java
@@ -0,0 +1,98 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.EstadoDAO;
+import br.pro.delfino.drogaria.dao.PessoaDAO;
+import br.pro.delfino.drogaria.domain.Cidade;
+import br.pro.delfino.drogaria.domain.Estado;
+import br.pro.delfino.drogaria.domain.Pessoa;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class PessoaBean implements Serializable {
+	private Pessoa pessoa;
+	private List<Pessoa> pessoas;
+
+	private List<Estado> estados;
+	private List<Cidade> cidades;
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	public List<Estado> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<Estado> estados) {
+		this.estados = estados;
+	}
+
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	@PostConstruct
+	public void listar() {
+		try {
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+		try {
+			pessoa = new Pessoa();
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+
+			cidades = new ArrayList<Cidade>();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar uma nova pessoa");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+
+	}
+
+	public void salvar() {
+
+	}
+
+	public void excluir(ActionEvent evento) {
+
+	}
+}
