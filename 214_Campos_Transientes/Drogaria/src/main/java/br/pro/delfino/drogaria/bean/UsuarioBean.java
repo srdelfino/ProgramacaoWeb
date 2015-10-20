9b2ae4575e319189534d3f96fe48b67c6a58c24d
srdelfino <srdelfino@gmail.com>
2015-10-20 15:25:41 -0200

    Atualizações

---
+++ 214_Campos_Transientes/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/UsuarioBean.java
@@ -0,0 +1,39 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.UsuarioDAO;
+import br.pro.delfino.drogaria.domain.Usuario;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class UsuarioBean implements Serializable {
+	private List<Usuario> usuarios;
+	
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	@PostConstruct
+	public void listar(){
+		try{
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			usuarios = usuarioDAO.listar("tipo");
+		}catch(RuntimeException erro){
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os usuários");
+			erro.printStackTrace();
+		}
+	}
+}
