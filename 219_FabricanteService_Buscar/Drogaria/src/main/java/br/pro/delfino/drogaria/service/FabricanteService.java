f9f8943020604c5397d3cef7f20f66af813f5870
srdelfino <srdelfino@gmail.com>
2015-10-21 11:31:18 -0200

    Atualizações

---
+++ 219_FabricanteService_Buscar/Drogaria/src/main/java/br/pro/delfino/drogaria/service/FabricanteService.java
@@ -0,0 +1,41 @@
+package br.pro.delfino.drogaria.service;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import com.google.gson.Gson;
+
+import br.pro.delfino.drogaria.dao.FabricanteDAO;
+import br.pro.delfino.drogaria.domain.Fabricante;
+
+@Path("fabricante")
+public class FabricanteService {
+	// http://127.0.0.1:8080/Drogaria/rest/fabricante
+	@GET
+	public String listar(){
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		List<Fabricante> fabricantes = fabricanteDAO.listar("descricao");
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(fabricantes);
+		
+		return json;
+	}
+	
+	// http://127.0.0.1:8080/Drogaria/rest/fabricante/{codigo}
+	// http://127.0.0.1:8080/Drogaria/rest/fabricante/10
+	@GET
+	@Path("{codigo}")
+	public String buscar(@PathParam("codigo") Long codigo){
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		Fabricante fabricante = fabricanteDAO.buscar(codigo);
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(fabricante);
+		
+		return json;
+	}
+}
