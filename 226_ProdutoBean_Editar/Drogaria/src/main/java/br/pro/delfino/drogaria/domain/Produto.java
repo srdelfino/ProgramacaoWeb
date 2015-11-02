f7f0bf4e5fd7632deef506f2c4d40e8a9c6b2279
srdelfino <srdelfino@gmail.com>
2015-11-02 14:08:17 -0200

    Atualizações

---
+++ 226_ProdutoBean_Editar/Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Produto.java
@@ -0,0 +1,57 @@
+package br.pro.delfino.drogaria.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@SuppressWarnings("serial")
+@Entity
+public class Produto extends GenericDomain {
+	@Column(length = 80, nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private Short quantidade;
+	
+	@Column(nullable = false, precision = 6, scale = 2)
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Fabricante fabricante;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Short getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Short quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+}
