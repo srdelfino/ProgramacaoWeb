e3d361c0981923fdd558412fe20b39e670bf72c0
srdelfino <srdelfino@gmail.com>
2015-11-02 16:50:51 -0200

    Atualizações

---
+++ 228_Upload_Fotos_Parte_2/Drogaria/src/test/java/br/pro/delfino/drogaria/dao/EstadoDAOTest.java
@@ -0,0 +1,88 @@
+package br.pro.delfino.drogaria.dao;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.pro.delfino.drogaria.domain.Estado;
+
+public class EstadoDAOTest {
+	@Test
+	@Ignore
+	public void salvar() {
+		Estado estado = new Estado();
+		estado.setNome("Rio Grande do Sul");
+		estado.setSigla("RS");
+
+		EstadoDAO estadoDAO = new EstadoDAO();
+		estadoDAO.salvar(estado);
+	}
+
+	@Test
+	@Ignore
+	public void listar() {
+		EstadoDAO estadoDAO = new EstadoDAO();
+		List<Estado> resultado = estadoDAO.listar();
+
+		System.out.println("Total de Registros Encontrados: " + resultado.size());
+
+		for (Estado estado : resultado) {
+			System.out.println(estado.getCodigo() + " - " + estado.getSigla() + " - " + estado.getNome());
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void buscar(){
+		Long codigo = 3L;
+		
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+		
+		if(estado == null){
+			System.out.println("Nenhum registro encontrado");
+		}else{
+			System.out.println("Registro encontrado:");
+			System.out.println(estado.getCodigo() + " - " + estado.getSigla() + " - " + estado.getNome());
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void excluir(){
+		Long codigo = 1L;
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+		
+		if(estado == null){
+			System.out.println("Nenhum registro encontrado");
+		}else{
+			estadoDAO.excluir(estado);
+			System.out.println("Registro removido:");
+			System.out.println(estado.getCodigo() + " - " + estado.getSigla() + " - " + estado.getNome());
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void editar(){
+		Long codigo = 10L;
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+		
+		if(estado == null){
+			System.out.println("Nenhum registro encontrado");
+		}else{
+			System.out.println("Registro editado - Antes:");
+			System.out.println(estado.getCodigo() + " - " + estado.getSigla() + " - " + estado.getNome());
+			
+			estado.setNome("Santa Catarina");
+			estado.setSigla("SC");
+			estadoDAO.editar(estado);
+			
+			System.out.println("Registro editado - Depois:");
+			System.out.println(estado.getCodigo() + " - " + estado.getSigla() + " - " + estado.getNome());
+		}
+	}
+}
