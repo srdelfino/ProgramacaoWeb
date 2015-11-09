432903147566b91f032867956a9157cc3c9d945a
srdelfino <srdelfino@gmail.com>
2015-11-08 23:51:14 -0200

    Atualizações

---
+++ 234_Venda_Produtos_Repetidos/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/VendaBean.java
@@ -0,0 +1,78 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.ProdutoDAO;
+import br.pro.delfino.drogaria.domain.ItemVenda;
+import br.pro.delfino.drogaria.domain.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class VendaBean implements Serializable {
+	private List<Produto> produtos;
+	private List<ItemVenda> itensVenda;
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public List<ItemVenda> getItensVenda() {
+		return itensVenda;
+	}
+
+	public void setItensVenda(List<ItemVenda> itensVenda) {
+		this.itensVenda = itensVenda;
+	}
+
+	@PostConstruct
+	public void listar() {
+		try {
+			ProdutoDAO produtoDAO = new ProdutoDAO();
+			produtos = produtoDAO.listar("descricao");
+
+			itensVenda = new ArrayList<>();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar carregar a tela de vendas");
+			erro.printStackTrace();
+		}
+	}
+
+	public void adicionar(ActionEvent evento) {
+		Produto produto = (Produto) evento.getComponent().getAttributes().get("produtoSelecionado");
+
+		int achou = -1;
+		for (int posicao = 0; posicao < itensVenda.size(); posicao++) {
+			if (itensVenda.get(posicao).getProduto().equals(produto)) {
+				achou = posicao;
+			}
+		}
+
+		if (achou < 0) {
+			ItemVenda itemVenda = new ItemVenda();
+			itemVenda.setPrecoParcial(produto.getPreco());
+			itemVenda.setProduto(produto);
+			itemVenda.setQuantidade(new Short("1"));
+			
+			itensVenda.add(itemVenda);
+		}else{
+			ItemVenda itemVenda = itensVenda.get(achou);
+			itemVenda.setQuantidade(new Short(itemVenda.getQuantidade() + 1 + ""));
+			itemVenda.setPrecoParcial(produto.getPreco().multiply(new BigDecimal(itemVenda.getQuantidade())));
+		}	
+	}
+}
