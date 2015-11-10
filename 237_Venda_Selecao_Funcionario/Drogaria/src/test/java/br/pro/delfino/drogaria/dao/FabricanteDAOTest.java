a8d119d49ece4e1301bf69ec5b40a43480f86ae7
srdelfino <srdelfino@gmail.com>
2015-11-10 00:42:54 -0200

    Atualizações

---
+++ 237_Venda_Selecao_Funcionario/Drogaria/src/test/java/br/pro/delfino/drogaria/dao/FabricanteDAOTest.java
@@ -0,0 +1,63 @@
+package br.pro.delfino.drogaria.dao;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.pro.delfino.drogaria.domain.Fabricante;
+
+public class FabricanteDAOTest {
+	@Test
+	@Ignore
+	public void salvar() {
+		Fabricante fabricante = new Fabricante();
+		fabricante.setDescricao("Aché");
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.salvar(fabricante);
+	}
+
+	@Test
+	@Ignore
+	public void listar() {
+		FabricanteDAO FabricanteDAO = new FabricanteDAO();
+		List<Fabricante> resultado = FabricanteDAO.listar();
+
+		System.out.println("Total de Registros Encontrados: " + resultado.size());
+
+		for (Fabricante fabricante : resultado) {
+			System.out.println(fabricante.getCodigo() + " - " + fabricante.getDescricao());
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void buscar(){
+		Long codigo = 3L;
+		
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		Fabricante fabricante = fabricanteDAO.buscar(codigo);
+		
+		if(fabricante == null){
+			System.out.println("Nenhum registro encontrado");
+		}else{
+			System.out.println("Registro encontrado:");
+			System.out.println(fabricante.getCodigo() + " - " + fabricante.getDescricao());
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void merge() {
+		//Fabricante fabricante = new Fabricante();
+		//fabricante.setDescricao("Fabricante A");
+		//FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		//fabricanteDAO.merge(fabricante);
+		
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		Fabricante fabricante = fabricanteDAO.buscar(5L);
+		fabricante.setDescricao("Fabricante B");
+		fabricanteDAO.merge(fabricante);
+	}
+}
