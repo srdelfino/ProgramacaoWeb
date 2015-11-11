aa01f39125fe3095d9b899f3d466626f5a710225
srdelfino <srdelfino@gmail.com>
2015-11-11 11:17:39 -0200

    Atualizações

---
+++ 243_Banner/Drogaria/src/main/java/br/pro/delfino/drogaria/dao/VendaDAO.java
@@ -0,0 +1,39 @@
+package br.pro.delfino.drogaria.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import br.pro.delfino.drogaria.domain.ItemVenda;
+import br.pro.delfino.drogaria.domain.Venda;
+import br.pro.delfino.drogaria.util.HibernateUtil;
+
+public class VendaDAO extends GenericDAO<Venda> {
+	public void salvar(Venda venda, List<ItemVenda> itensVenda){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		Transaction transacao = null;
+
+		try {
+			transacao = sessao.beginTransaction();
+		
+			sessao.save(venda);
+			
+			for(int posicao = 0; posicao < itensVenda.size(); posicao++){
+				ItemVenda itemVenda = itensVenda.get(posicao);
+				itemVenda.setVenda(venda);
+				
+				sessao.save(itemVenda);
+			}
+			
+			transacao.commit();
+		} catch (RuntimeException erro) {
+			if (transacao != null) {
+				transacao.rollback();
+			}
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
+}
