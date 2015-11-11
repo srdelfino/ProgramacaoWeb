e8b41891ffef7d336767dd710bddb498686c1be7
srdelfino <srdelfino@gmail.com>
2015-11-11 21:59:21 -0200

    Atualizações

---
+++ 247_Facelets_Composição/Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Cliente.java
@@ -0,0 +1,49 @@
+package br.pro.delfino.drogaria.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@SuppressWarnings("serial")
+@Entity
+public class Cliente extends GenericDomain {
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dataCadastro;
+	
+	@Column(nullable = false)
+	private Boolean liberado;
+	
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Pessoa pessoa;
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Boolean getLiberado() {
+		return liberado;
+	}
+
+	public void setLiberado(Boolean liberado) {
+		this.liberado = liberado;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+}
