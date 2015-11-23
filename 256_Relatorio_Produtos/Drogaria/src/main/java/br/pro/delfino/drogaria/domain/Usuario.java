6a6745d293f1c9b9a4167d32440f2b32f915d36e
srdelfino <srdelfino@gmail.com>
2015-11-23 18:22:00 -0200

    Atualizações

---
+++ 256_Relatorio_Produtos/Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Usuario.java
@@ -0,0 +1,82 @@
+package br.pro.delfino.drogaria.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Transient;
+
+@SuppressWarnings("serial")
+@Entity
+public class Usuario extends GenericDomain {
+	@Column(length = 32, nullable = false)
+	private String senha;
+
+	@Column(nullable = false)
+	private Character tipo;
+
+	@Column(nullable = false)
+	private Boolean ativo;
+
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Pessoa pessoa;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Character getTipo() {
+		return tipo;
+	}
+
+	@Transient
+	public String getTipoFormatado() {
+		String tipoFormatado = null;
+
+		if (tipo == 'A') {
+			tipoFormatado = "Administrador";
+		} else if (tipo == 'B') {
+			tipoFormatado = "Balconista";
+		} else if (tipo == 'G') {
+			tipoFormatado = "Gerente";
+		}
+		
+		return tipoFormatado;
+	}
+
+	public void setTipo(Character tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	
+	@Transient
+	public String getAtivoFormatado(){
+		String ativoFormatado = "Não";
+		
+		if(ativo){
+			ativoFormatado = "Sim";
+		}
+		
+		return ativoFormatado;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+}
