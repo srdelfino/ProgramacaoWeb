ecdaa8c9535239e47c8c2df704424d9decc57ea4
srdelfino <srdelfino@gmail.com>
2015-11-25 10:44:50 -0200

    Atualizações

---
+++ 224_RestClient_Listar/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/FabricanteBean.java
@@ -0,0 +1,103 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.omnifaces.util.Messages;
+
+import com.google.gson.Gson;
+
+import br.pro.delfino.drogaria.dao.FabricanteDAO;
+import br.pro.delfino.drogaria.domain.Fabricante;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class FabricanteBean implements Serializable {
+	private Fabricante fabricante;
+	private List<Fabricante> fabricantes;
+
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public List<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	public void setFabricantes(List<Fabricante> fabricantes) {
+		this.fabricantes = fabricantes;
+	}
+
+	@PostConstruct
+	public void listar() {
+		try {
+			//FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			//fabricantes = fabricanteDAO.listar();
+			
+			Client cliente = ClientBuilder.newClient();
+			WebTarget caminho = cliente.target("http://127.0.0.1:8080/Drogaria/rest/fabricante");
+			String json = caminho.request().get(String.class);
+			
+			Gson gson = new Gson();
+			Fabricante[] vetor = gson.fromJson(json, Fabricante[].class);
+			
+			fabricantes = Arrays.asList(vetor);
+ 		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os fabricantes");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+		fabricante = new Fabricante();
+	}
+
+	public void salvar() {
+		try {
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.merge(fabricante);
+
+			fabricante = new Fabricante();
+			fabricantes = fabricanteDAO.listar();
+
+			Messages.addGlobalInfo("Fabricante salvo com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o fabricante");
+			erro.printStackTrace();
+		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
+
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.excluir(fabricante);
+
+			fabricantes = fabricanteDAO.listar();
+
+			Messages.addGlobalInfo("Fabricante removido com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover o fabricante");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+		fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
+	}
+}
