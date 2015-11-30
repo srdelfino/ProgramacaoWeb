885ea4aaede6de629f5c17556d624d78d7840616
srdelfino <srdelfino@gmail.com>
2015-11-30 13:01:42 -0200

    Atualizações

---
+++ 264_Busca_Produtos_Parte_3/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean2.java
@@ -0,0 +1,60 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.ProdutoDAO;
+import br.pro.delfino.drogaria.domain.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class ProdutoBean2 implements Serializable {
+	private Produto produto;
+	private Boolean exibePainelDados;
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	public Boolean getExibePainelDados() {
+		return exibePainelDados;
+	}
+	
+	public void setExibePainelDados(Boolean exibePainelDados) {
+		this.exibePainelDados = exibePainelDados;
+	}
+
+	@PostConstruct
+	public void novo() {
+		produto = new Produto();
+		exibePainelDados = false;
+	}
+
+	public void buscar() {
+		try {
+			ProdutoDAO produtoDAO = new ProdutoDAO();
+			Produto resultado = produtoDAO.buscar(produto.getCodigo());
+
+			if (resultado == null) {
+				exibePainelDados = false;
+				Messages.addGlobalWarn("Não existe produto cadastrado para o código informado");
+			} else {
+				exibePainelDados = true;
+				produto = resultado;
+			}
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar buscar o produto");
+			erro.printStackTrace();
+		}
+	}
+}
