3ef8d9ea05696fa611502b96a5c41ca955042852
srdelfino <srdelfino@gmail.com>
2015-12-02 11:44:27 -0200

    Atualizações

---
+++ 266_Historico_Salvar_Parte_2/Drogaria/src/test/java/br/pro/delfino/drogaria/dao/UsuarioDAOTest.java
@@ -0,0 +1,31 @@
+package br.pro.delfino.drogaria.dao;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.pro.delfino.drogaria.domain.Pessoa;
+import br.pro.delfino.drogaria.domain.Usuario;
+
+public class UsuarioDAOTest {
+	@Test
+	@Ignore
+	public void salvar(){
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		Pessoa pessoa = pessoaDAO.buscar(1L);
+		
+		System.out.println("Pessoa Encontrada");
+		System.out.println("Nome: " + pessoa.getNome());
+		System.out.println("CPF: " + pessoa.getCpf());
+		
+		Usuario usuario = new Usuario();
+		usuario.setAtivo(true);
+		usuario.setPessoa(pessoa);
+		usuario.setSenha("q1w2e3r4");
+		usuario.setTipo('A');
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.salvar(usuario);
+		
+		System.out.println("Usuário salvo com sucesso.");
+	}
+}	
