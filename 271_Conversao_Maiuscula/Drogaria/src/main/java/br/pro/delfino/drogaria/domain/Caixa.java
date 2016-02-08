0514fd312426d037be2adc8e7bb3daf0d299ee8d
srdelfino <srdelfino@gmail.com>
2016-02-07 23:45:27 -0200

    Atualizações

---
+++ 271_Conversao_Maiuscula/Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Caixa.java
@@ -0,0 +1,48 @@
+package br.pro.delfino.drogaria.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@SuppressWarnings("serial")
+@Entity
+public class Caixa extends GenericDomain {
+	@Column(nullable = false, unique = true)
+	@Temporal(TemporalType.DATE)
+	private Date dataAbertura;
+
+	@Column(nullable = true)
+	@Temporal(TemporalType.DATE)
+	private Date dataFechamento;
+
+	@Column(nullable = false, precision = 7, scale = 2)
+	private BigDecimal valorAbertura;
+
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public Date getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(Date dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public BigDecimal getValorAbertura() {
+		return valorAbertura;
+	}
+	
+	public void setValorAbertura(BigDecimal valorAbertura) {
+		this.valorAbertura = valorAbertura;
+	}
+}
