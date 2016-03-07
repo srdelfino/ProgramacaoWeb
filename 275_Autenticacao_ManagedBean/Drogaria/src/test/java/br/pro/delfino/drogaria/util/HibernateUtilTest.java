a356bb522558e67827eb1fe7cedfd9971e937667
srdelfino <srdelfino@gmail.com>
2016-03-06 21:59:38 -0300

    Atualizações

---
+++ 275_Autenticacao_ManagedBean/Drogaria/src/test/java/br/pro/delfino/drogaria/util/HibernateUtilTest.java
@@ -0,0 +1,13 @@
+package br.pro.delfino.drogaria.util;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+	@Test
+	public void conectar(){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		sessao.close();
+		HibernateUtil.getFabricaDeSessoes().close();
+	}
+}
