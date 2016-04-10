6d559d46b56fc7a74583051612b7248f370ca005
srdelfino <srdelfino@gmail.com>
2016-04-10 18:43:30 -0300

    Aula 285

---
+++ 285_Cadastro_Produto_Listagem/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean2.java
@@ -0,0 +1,44 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.ProdutoDAO;
+import br.pro.delfino.drogaria.domain.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class ProdutoBean2  implements Serializable {
+	private List<Produto> produtos;
+	
+	private ProdutoDAO produtoDAO;
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	@PostConstruct
+	public void iniciar(){
+		produtoDAO = new ProdutoDAO();
+	}
+	
+	public void listar() {
+		try {
+			produtos = produtoDAO.listar("descricao");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os produtos");
+			erro.printStackTrace();
+		}
+	}
+}
---
+++ 285_Cadastro_Produto_Listagem/Drogaria/src/main/java/br/pro/delfino/drogaria/dao/UsuarioDAO.java
@@ -0,0 +1,33 @@
+package br.pro.delfino.drogaria.dao;
+
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import br.pro.delfino.drogaria.domain.Usuario;
+import br.pro.delfino.drogaria.util.HibernateUtil;
+
+public class UsuarioDAO extends GenericDAO<Usuario> {
+	public Usuario autenticar(String cpf, String senha) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		
+		try{
+			Criteria consulta = sessao.createCriteria(Usuario.class);
+			consulta.createAlias("pessoa", "p");
+			
+			consulta.add(Restrictions.eq("p.cpf", cpf));
+			
+			SimpleHash hash = new SimpleHash("md5", senha);
+			consulta.add(Restrictions.eq("senha", hash.toHex()));
+			
+			Usuario resultado = (Usuario) consulta.uniqueResult();
+			
+			return resultado;
+		} catch (RuntimeException erro) {
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
+}
