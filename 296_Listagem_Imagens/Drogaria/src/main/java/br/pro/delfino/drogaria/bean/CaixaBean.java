1eb6fdc23fd785293eba92730a8af1eb4917cc82
srdelfino <srdelfino@gmail.com>
2016-05-13 17:58:16 -0300

    Atualizações

---
+++ 296_Listagem_Imagens/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/CaixaBean.java
@@ -0,0 +1,76 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleModel;
+
+import br.pro.delfino.drogaria.dao.CaixaDAO;
+import br.pro.delfino.drogaria.dao.FuncionarioDAO;
+import br.pro.delfino.drogaria.domain.Caixa;
+import br.pro.delfino.drogaria.domain.Funcionario;
+
+@ManagedBean
+@ViewScoped
+public class CaixaBean {
+	private Caixa caixa;
+
+	private ScheduleModel caixas;
+	private List<Funcionario> funcionarios;
+
+	public Caixa getCaixa() {
+		return caixa;
+	}
+
+	public void setCaixa(Caixa caixa) {
+		this.caixa = caixa;
+	}
+
+	public ScheduleModel getCaixas() {
+		return caixas;
+	}
+
+	public void setCaixas(ScheduleModel caixas) {
+		this.caixas = caixas;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	@PostConstruct
+	public void listar() {
+		caixas = new DefaultScheduleModel();
+	}
+
+	public void novo(SelectEvent evento) {
+		caixa = new Caixa();
+		caixa.setDataAbertura((Date) evento.getObject());
+
+		FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+		funcionarios = funcionarioDAO.listar();
+	}
+	
+	public void salvar(){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(caixa.getDataAbertura());
+		calendar.add(Calendar.DATE, 1);
+		caixa.setDataAbertura(calendar.getTime());
+		
+		CaixaDAO caixaDAO = new CaixaDAO();
+		caixaDAO.salvar(caixa);
+		Messages.addGlobalInfo("Caixa aberto com sucesso");
+	}
+}
