5c3bfd81942595ba6e45ac34bc964dcecfeb1ca0
srdelfino <srdelfino@gmail.com>
2016-08-28 23:23:16 -0300

    Atualizações

---
+++ 309_Introducao_Internacionalizacao/Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Venda.java
@@ -0,0 +1,75 @@
+package br.pro.delfino.drogaria.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@SuppressWarnings("serial")
+@Entity
+public class Venda extends GenericDomain {
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date horario;
+	
+	@Column(nullable = false, precision = 7, scale = 2)
+	private BigDecimal precoTotal;
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Funcionario funcionario;
+	
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "venda")
+	private List<ItemVenda> itensVenda;
+
+	public Date getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+
+	public BigDecimal getPrecoTotal() {
+		return precoTotal;
+	}
+
+	public void setPrecoTotal(BigDecimal precoTotal) {
+		this.precoTotal = precoTotal;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	public List<ItemVenda> getItensVenda() {
+		return itensVenda;
+	}
+	
+	public void setItensVenda(List<ItemVenda> itensVenda) {
+		this.itensVenda = itensVenda;
+	}
+}
