efeef7750e13a4acf9acd8c0e767452267880352
srdelfino <srdelfino@gmail.com>
2016-08-29 00:18:19 -0300

    Atualizações

---
+++ 311_Troca_Idioma_Mensagens/Drogaria/src/main/java/br/pro/delfino/drogaria/bean/UsuarioBean.java
@@ -0,0 +1,91 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.PessoaDAO;
+import br.pro.delfino.drogaria.dao.UsuarioDAO;
+import br.pro.delfino.drogaria.domain.Pessoa;
+import br.pro.delfino.drogaria.domain.Usuario;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class UsuarioBean implements Serializable {
+	private Usuario usuario;
+	
+	private List<Pessoa> pessoas;
+	private List<Usuario> usuarios;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+	
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	@PostConstruct
+	public void listar(){
+		try{
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			usuarios = usuarioDAO.listar("tipoUsuario");
+		}catch(RuntimeException erro){
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os usuários");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void novo() {
+		try {
+			usuario = new Usuario();
+
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar("nome");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar criar um novo usuário");
+			erro.printStackTrace();
+		}
+	}
+
+	public void salvar() {
+		try {
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			usuarioDAO.merge(usuario);
+			
+			usuario = new Usuario();
+			usuarios = usuarioDAO.listar("tipoUsuario");
+			
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar("nome");
+			
+			Messages.addGlobalInfo(Faces.getResourceBundle("msg").getString("usuarioSalvo"));
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o usuário");
+			erro.printStackTrace();
+		}
+	}
+}
